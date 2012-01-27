f7ed086b57d8ac0c877156d9a59ec4906709c917
Arnaud MARTEL <arnaud@aclnr.net>
2012-01-27 10:37:07 +0100

    Optimization for projects where module is not enabled

--- lib/bioproj_user_patch.rb
+++ lib/bioproj_user_patch.rb
@@ -13,24 +13,26 @@ end
 module UserMethodsBioproj
   def roles_for_project_with_bioproj(project)
     roles = roles_for_project_without_bioproj(project)
-    @role_non_member ||= Role.non_member
-    @role_anonymous ||= Role.anonymous
+    if project.module_enabled?('bioproj')
+      @role_non_member ||= Role.non_member
+      @role_anonymous ||= Role.anonymous
 
-    if roles[0] == @role_anonymous
-      setting = BioprojSetting.find_or_create project
-      if setting.anonymous_role_id > 1
-        roles = []
-        roles << Role.find(setting.anonymous_role_id - 2)
-      elsif setting.anonymous_role_id == 1
-        roles = []
-      end
-    elsif roles[0] == @role_non_member
-      setting = BioprojSetting.find_or_create project
-      if setting.non_member_role_id > 1
-        roles = []
-        roles << Role.find(setting.non_member_role_id - 2)
-      elsif setting.non_member_role_id == 1
-        roles = []
+      if roles[0] == @role_anonymous
+        setting = BioprojSetting.find_or_create project
+        if setting.anonymous_role_id > 1
+          roles = []
+          roles << Role.find(setting.anonymous_role_id - 2)
+        elsif setting.anonymous_role_id == 1
+          roles = []
+        end
+      elsif roles[0] == @role_non_member
+        setting = BioprojSetting.find_or_create project
+        if setting.non_member_role_id > 1
+          roles = []
+          roles << Role.find(setting.non_member_role_id - 2)
+        elsif setting.non_member_role_id == 1
+          roles = []
+        end
       end
     end
     roles
