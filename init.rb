c59c2772e51fbe0bc946b33ac896b7e86a76a955
Arnaud MARTEL <arnaud@aclnr.net>
2012-08-21 09:37:05 +0200

    redmine 2.0.3 now required

--- init.rb
+++ init.rb
@@ -7,9 +7,9 @@ end
 Redmine::Plugin.register :redmine_bioproj do
   name 'BIOPROJ plugin'
   author 'Arnaud Martel'
-  description 'Fonctionnalités additionnelles pour BIOPROJ'
-  version '0.0.2'
-  requires_redmine :version_or_higher => '1.3.0'
+  description 'Additional features used in our BIOPROJ application'
+  version '0.0.3'
+  requires_redmine :version_or_higher => '2.0.3'
   
   project_module :bioproj do
     permission :bioproj_members, {:bioproj_members => [:show, :edit_membership, :destroy_membership, :access]}
