45e0e7f952cedefa492b2aea252dbf8a282e9e64
Arnaud MARTEL <arnaud@aclnr.net>
2014-02-04 16:25:57 +0100

    send email to the first forum

--- lib/bioproj_mail_handler_patch.rb
+++ lib/bioproj_mail_handler_patch.rb
@@ -25,26 +25,32 @@ module ForumMailHandlerPatch
 
   module ForumMailHandlerInstanceMethods
     def dispatch_with_forums
-      if m = email.subject.match(/(\[)(.*)( - )(.*)(\])/)
+      if m = email.subject.match(/(\[)(.*)(\])/)
         project = Project.find_by_name(m[2].strip)
-        board = project.boards.find_by_name(m[4].strip) unless project.nil?
-        if !board.nil?
-          if email.subject.match(/re: /i)
-            dispatch_without_forums
-          else
-            project = board.project
-            # check permission
-            raise MailHandler::UnauthorizedAction unless user.allowed_to?(:edit_messages, project)
-            message = Message.new(:author => user, :board => board)
-            message.subject = email.subject.split(/(\[)(.*)( - )(.*)(\])/)[6].strip
-            message.content = plain_text_body
-            message.save!
-            add_attachments(message)
-            logger.info "MailHandler: message ##{message.id} - #{message.subject} created by #{user}" if logger && logger.info
-            message
-          end
+        if !project.nil?
+          board = project.boards.first unless project.nil?
         else
+          if m = email.subject.match(/(\[)(.*)( - )(.*)(\])/)
+            project = Project.find_by_name(m[2].strip)
+            board = project.boards.find_by_name(m[4].strip) unless project.nil?
+          end
+        end
+      end
+      
+      if !board.nil?
+        if email.subject.match(/re: /i)
           dispatch_without_forums
+        else
+          project = board.project
+          # check permission
+          raise MailHandler::UnauthorizedAction unless user.allowed_to?(:edit_messages, project)
+          message = Message.new(:author => user, :board => board)
+          message.subject = email.subject.gsub(/\[[^\]]*\]/,'')
+          message.content = plain_text_body
+          message.save!
+          add_attachments(message)
+          logger.info "MailHandler: message ##{message.id} - #{message.subject} created by #{user}" if logger && logger.info
+          message
         end
       else
         dispatch_without_forums
